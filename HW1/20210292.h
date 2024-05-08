f55dbf0fad6dc85bedc29d381bcc34b53c9cc34d
Huiyeongkim <huiyeong9619@naver.com>
2024-05-08 10:07:33 +0900

    commit

---
+++ HW1/20210292.h
@@ -0,0 +1,13 @@
+#ifndef HEARDER_H
+#define HEARDER_H
+
+void toSignedChar(const char* buffer);
+void toAscii(const char* buffer);
+void toUnsignedChar(const char* buffer);
+void toSignedInt(const char* buffer);
+void toUnsignedInt(const char* buffer);
+void toFloat(const char* buffer);
+void toDouble(const char* buffer);
+long binaryToDecimal(const char* buffer,int i, int b);
+
+#endif
